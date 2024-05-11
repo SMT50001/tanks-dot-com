691fc18c1ec498a8a466fc256c31d665213604e9
SMT50001 <arrow4496@yandex.ru>
2024-05-11 21:26:10 +0300

    New input handling code, fixes

--- src/tanks.asm
+++ src/tanks.asm
@@ -8,35 +8,64 @@ org 100h
     push cs
     pop ds
 
+    call test_vga_card
+    cmp ax, 0
+    je .no_bios_support
+    cmp ax, 1
+    je .card_detect_failed
+
+    call disable_kbd_int
+
     call get_mode
     mov [mode_before_start], ax
     call set_vga_x_mode
     fastcall set_palette, palette
-    fastcall screen_fill, 1
 
+    set_curr_vid_page 0
+    fastcall plane_fill, 1
+
+    ; Do logic
     .main_loop:
-    call wait_retrace
-    fastcall draw_sprite, 152, [sprite_pos], tank1_0
     call get_key
-    cmp ah, 1
-    je .exit
-    cmp ah, 11h
+    cmp al, 1
+    je .stop_loop
+    cmp al, 11h
     jne .dont_move_sprite
-    fastcall draw_sprite, 152, [sprite_pos], tank3_3
-    sub [sprite_pos], 16
+    sub [sprite_pos], 1
     .dont_move_sprite:
-    call flush_kbd_buf
+    ; Wait for retrace and draw
+    call wait_retrace
+    fastcall redraw_sprite, 152, [sprite_pos], tank1_0
     jmp .main_loop
 
-    .exit:
+    .stop_loop:
+    call enable_kbd_int
     fastcall set_standart_mode, [mode_before_start]
+    jmp .exit
+
+    .no_bios_support:
+    mov dx, no_bios_support_msg
+    jmp .errors
+
+    .card_detect_failed:
+    mov dx, no_vga_msg
+    jmp .errors
+
+    .errors:
+    mov ah, 9h
+    int 21h
+
+    .exit:
     ret
 
 include 'vgax.inc'
 include 'gfxutil.inc'
 include 'gfx.inc'
 include 'input.inc'
+include 'v_logic.inc'
 
 mode_before_start dw 0
+no_bios_support_msg db 13, 10, 'No BIOS support detected', 13, 10, '$'
+no_vga_msg db 13, 10, 'No VGA card detected', 13, 10, '$'
 
 sprite_pos dw 224
